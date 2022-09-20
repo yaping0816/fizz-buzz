e285390a19b847b3b0094b406fbfe73d080282f4
yaping0816 <yaping0816@gmail.com>
2022-09-20 11:03:34 -0700

    Added junit test

--- src/main/java/com/tlglearning/FizzBuzz.java
+++ src/main/java/com/tlglearning/FizzBuzz.java
@@ -8,8 +8,8 @@ public enum FizzBuzz {
   FIZZ, BUZZ;
 
   public static Set<FizzBuzz> valueOf(int num) {
-    Set<FizzBuzz> result = EnumSet.noneOf(
-        FizzBuzz.class); //when we need to pass a class as an argument, we use .class after the class name, every calss hava a cclass field
+    Set<FizzBuzz> result = EnumSet.noneOf(FizzBuzz.class);
+    //when we need to pass a class as an argument, we use .class after the class name, every calss hava a cclass field
     if (num % 3 == 0) {
       result.add(FIZZ);
     }
--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -2,11 +2,16 @@ package com.tlglearning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
   @Test
   void valueOf() {
+//    assert Set.of(FizzBuzz.FIZZ).equals(FizzBuzz.valueOf(6)); //assert must follows by a boolean expression
+//    assert won't enabled in our code, only enabled automatically in test code
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6)); //first arguments is what we expect, second is the method invocation
   }
+
 }
