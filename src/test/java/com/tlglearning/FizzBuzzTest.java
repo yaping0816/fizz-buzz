6538842456658f0718f18d4ae159d5886122883e
yaping0816 <yaping0816@gmail.com>
2022-09-20 12:37:08 -0700

    Added more test cases for different possible outcomes.

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -8,10 +8,24 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
   @Test
-  void valueOf() {
-//    assert Set.of(FizzBuzz.FIZZ).equals(FizzBuzz.valueOf(6)); //assert must follows by a boolean expression
-//    assert won't enabled in our code, only enabled automatically in test code
+  void valueOf_fizz() {
     assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6)); //first arguments is what we expect, second is the method invocation
   }
+
+  @Test
+  void valueOf_buzz() {
+    assertEquals(Set.of(FizzBuzz.BUZZ), FizzBuzz.valueOf(10)); //compare contents/state. not address, assertSame check the objects, arre they point to the same address
+  }
+
+  @Test
+  void valueOf_fizzBuzz() {
+    assertEquals(Set.of(FizzBuzz.FIZZ, FizzBuzz.BUZZ), FizzBuzz.valueOf(15)); //compare contents/state. not address, assertSame check the objects, arre they point to the same address
+  }
+
+  @Test
+  void valueOf_none(){
+    assertEquals(Set.of(), FizzBuzz.valueOf(7));
+  }
+
 
 }
